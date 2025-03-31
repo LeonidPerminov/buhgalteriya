6e15793dd8232d5ef629fa6e9fb2ea5a63ee4211
Leonid Perminov <lperminov@mail.ru>
2025-03-31 12:40:34 +0300

    Добавил dirty_main.py с импортами через *

---
+++ dirty_main.py
@@ -0,0 +1,9 @@
+from application.salary import *
+from application.db.people import *
+from datetime import datetime
+
+if __name__ == '__main__':
+    print(f"Дата запуска: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    calculate_salary()
+    get_employees()
+    
